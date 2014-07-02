f122f7fc52e7583adbbc0184cc9a6f99518862a7
Greg Coonrod <coogr02@ca.com>
2014-07-02 13:44:55 -0500

    Updated config files and exec plugin script.

--- scripts/openshift.sh
+++ scripts/openshift.sh
@@ -1,53 +1,30 @@
-# This script is tailored to collectd openshift specfic metrics
+#!/bin/bash
+# This script collects simplifed cpu and disk metrics
 
-INTERVAL=10
+
 HOSTNAME="${COLLECTD_HOSTNAME:-localhost}"
+INTERVAL="${COLLECTD_INTERVAL:-10}"
 
-while `true`
-do
-  # USED=$(oo-cgroup-read memory.usage_in_bytes)
-  # MAX_USED=$(oo-cgroup-read memory.max_usage_in_bytes)
-  # LIMIT=$(oo-cgroup-read memory.limit_in_bytes)
-  # echo "PUTVAL \"$HOSTNAME/gear_memory/gear_memory\" interval=$INTERVAL N:$USED:$MAX_USED:$LIMIT"
+prev_total=0
+prev_idle=0
+while sleep "$INTERVAL"; do
+  cpu=`cat /proc/stat | head -n1 | sed 's/cpu //'`
+  user=`echo $cpu | awk '{print $1}'`
+  system=`echo $cpu | awk '{print $2}'`
+  nice=`echo $cpu | awk '{print $3}'`
+  idle=`echo $cpu | awk '{print $4}'`
+  wait=`echo $cpu | awk '{print $5}'`
+  irq=`echo $cpu | awk '{print $6}'`
+  srq=`echo $cpu | awk '{print $7}'`
+  zero=`echo $cpu | awk '{print $8}'`
+  total=$(($user+$system+$nice+$idle+$wait+$irq+$srq+$zero))
+  diff_idle=$(($idle-$prev_idle))
+  diff_total=$(($total-$prev_total))
+  usage=$(($((1000*$(($diff_total-$diff_idle))/$diff_total+5))/10))
+  echo "PUTVAL $HOSTNAME/cpu_util/cpu_util interval=$INTERVAL N:$usage"
+  prev_total=$total
+  prev_idle=$idle
 
-  VALUE=$(oo-cgroup-read cpuacct.stat)
-  SYS=`echo $VALUE | cut -f 4 -d " "`
-  USER=`echo $VALUE |cut -f 2 -d " "`
-  echo "PUTVAL \"$HOSTNAME/gear_cpu_user/gear_cpu_user\" interval=$INTERVAL N:$USER"
-  echo "PUTVAL \"$HOSTNAME/gear_cpu_system/gear_cpu_system\" interval=$INTERVAL N:$SYS"
-
-  # VALUE=`netstat -npt | grep tcp | grep -v "-" | awk '{ print $6}' | sort | uniq -c`
-  # ESTABLISHED=`echo $VALUE | grep ESTABLISHED | awk '{print $1 }'`
-  # CLOSED_WAIT=`echo $VALUE | grep CLOSED_WAIT | awk '{print $1 }'`
-  # TIME_WAIT=`echo $VALUE | grep TIME_WAIT | awk '{print $1 }'`
-  # LISTENING=`netstat -lnpt | grep -v "-" | grep tcp | wc -l`
-
-  # echo "PUTVAL \"$HOSTNAME/gear_network/gear_connections\" interval=$INTERVAL N:${ESTABLISHED:-0}:${TIME_WAIT:-0}:${CLOSED_WAIT:-0}:${LISTENING:-0}"
- 
-  # OLD_IFS=$IFS
-  # IFS=$'\n'
-  # DSS_CLI=`env | grep "DSS" | grep "CLI"`
-  # for CLI in $DSS_CLI
-  # do
-  #   HOST=`echo $CLI | cut -f 1 -d "=" | sed -e 's/_CLI//g'`
-  #   E=`echo $CLI | cut -f 1 -d "="`
-  #   K=`eval echo "/usr/bin/time --format \'%R\' \\\$\$E -e \'quit\' 2>&1"`
-  #   R=`eval $K 2>&1`
-  #   echo "PUTVAL \"$HOSTNAME/conn_to_$HOST/conn_time\" interval=$INTERVAL N:$R"
-  # done 
-  # IFS=OLD_IFS
-
- 
-# This captures lots of details about memory usage on the gear, perhaps a bit too much
-#  VALUE=$(oo-cgroup-read memory.stat)
-#  OLD_IFS=IFS;
-#  IFS=$'\n'
-#  for STAT in $VALUE
-#  do
-#    V=`echo $STAT | cut -f 2 -d " "`
-#    N=`echo $STAT | cut -f 1 -d " "`
-#    echo "PUTVAL \"$HOSTNAME/gear_memory_$N/counter\" interval=$INTERVAL N:$V"
-#  done
-#  IFS=OLD_IFS;
-  sleep $INTERVAL
+  iops=`iostat -d | awk 'NR <= 3 {next};{SUM += $2} END {print SUM}'`
+  echo "PUTVAL $HOSTNAME/disk_iops/disk_iops interval=$INTERVAL N:$iops"
 done
